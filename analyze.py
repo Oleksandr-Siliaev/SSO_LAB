3af253e22b467e13170124090586403b820dfd22
Oleksandr-Siliaev <115369769+Oleksandr-Siliaev@users.noreply.github.com>
2025-04-17 12:32:40 +0300

    Create analyze.py

---
+++ analyze.py
@@ -0,0 +1,17 @@
+import os
+from radon.complexity import cc_visit
+from radon.metrics import h_visit
+from radon.raw import analyze
+
+with open("calculator.py") as f:
+    code = f.read()
+
+print("Цикломатична складність:")
+for block in cc_visit(code):
+    print(block)
+
+print("\nМетрики Холстеда:")
+print(h_visit(code))
+
+print("\nSLOC:")
+print(analyze(code))
