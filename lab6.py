27acabc0af95194739f3261b9421883ad1449b5c
Oleksandr-Siliaev <115369769+Oleksandr-Siliaev@users.noreply.github.com>
2025-04-17 13:01:57 +0300

    style

--- lab6.py
+++ lab6.py
@@ -5,7 +5,16 @@ def cyclomatic_complexity(code: str) -> int:
     tree = ast.parse(code)
     complexity = 1  # Start with 1 for the default control flow
     for node in ast.walk(tree):
-        if isinstance(node, (ast.If, ast.For, ast.While, ast.Try, ast.ExceptHandler)):
+        if isinstance(
+            node,
+            (
+                ast.If,
+                ast.For,
+                ast.While,
+                ast.Try,
+                ast.ExceptHandler,
+            ),
+        ):
             complexity += 1
     return complexity
 
